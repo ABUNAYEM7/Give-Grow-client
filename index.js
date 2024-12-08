17ce077236c60a92bec0dc26c9c90ac64ded1b0a
Mohammad Abu Nayem <mohammadabunayem106@gmail.com>
2024-12-08 11:01:45 +0600

    Add dotenv fie

--- index.js
+++ index.js
@@ -149,12 +149,12 @@ async function run() {
           res.send(result)
         })
 
-        app.get('/AllCampaign/:id',async(req,res)=>{
-          const id = req.params.id;
-          const query = {_id :new ObjectId(id)}
-          const result =await campaignCollection.find(query).toArray()
-          res.send(result)
-        })
+        // app.get('/AllCampaign/:id',async(req,res)=>{
+        //   const id = req.params.id;
+        //   const query = {_id :new ObjectId(id)}
+        //   const result =await campaignCollection.find(query).toArray()
+        //   res.send(result)
+        // })
 
         app.get('/donation/:email',async(req,res)=>{
             const email = req.params.email;
