6cacfc940ac09c206be7494c8264a5580b90892d
Mohammad Abu Nayem <mohammadabunayem106@gmail.com>
2024-12-18 17:15:05 +0600

    add simple jwt opearation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -112,16 +114,30 @@ const campaigns = [
 
 
 
-app.use(cors())
-
+app.use(cors({
+  origin:['http://localhost:5173','https://give-n-grow.web.app'],
+  credentials:true
+}))
 
 app.use(express.json())
+app.use(cookieParser())
+
+const verifyToken =(req,res,next)=>{
+  const token = req.cookies?.token
+  if(!token){
+    return res.status(401).send({message : 'unauthorized access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({message : 'unauthorized access'})
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qcus7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -156,18 +172,50 @@ async function run() {
           res.send(result)
         })
 
-        app.get('/donation/:email',async(req,res)=>{
+        app.get('/donation/:email',verifyToken,async(req,res)=>{
             const email = req.params.email;
+
+            if(req.user.email !== email){
+              return res.status(403).send('Forbidden Access')
+            }
+
             const cursor = {email :email}
             const result =await  donationCollection.find(cursor).toArray()
             res.send(result)
         })
 
-        app.get('/myCampaign/:email',async(req,res)=>{
+        app.get('/myCampaign/:email',verifyToken,async(req,res)=>{
           const email = req.params.email;
+          if(req.user.email !== email){
+            return res.status(403).send('Forbidden Access')
+          }
           const cursor = {email: email}
           const result = await campaignCollection.find(cursor).toArray()
           res.send(result)
+        })
+
+        // cookie-signIn-post
+        app.post('/jwt',(req,res)=>{
+          const user = req.body;
+          const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn : '1d'})
+          res
+          .cookie('token',token,{
+            httpOnly : true,
+            secure:process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+          })
+          .send({success : true})
+        })
+
+        // clear-cookie-logout-post
+        app.post('/logout',(req,res)=>{
+          res 
+          .clearCookie('token',{
+            httpOnly :true,
+            secure : process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+          })
+          .send({success : true})
         })
 
         app.post('/donation',async(req,res)=>{
@@ -212,7 +260,7 @@ async function run() {
         })
 
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // await client.close();
   }
